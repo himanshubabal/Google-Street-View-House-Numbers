7750ba8d5f933fd69133e7aaf511cd32d49fcf46
Himanshu Babal <babal.himanshu@gmail.com>
2017-02-08 16:28:19 +0530

    Ready for submission

--- get_data.py
+++ get_data.py
@@ -11,13 +11,13 @@ from download_helper.SVHN_multi_box import prep_svhn_multi_box
 SVHN_new_data_struct_id = '0B4jlyZGFzRIJampwNlk1MjZNTzA'
 SVHN_new_data_struct_name = 'SVHN_new_data_struct.pickle'
 
-MNIST_multi_id = '0B4jlyZGFzRIJdmw0RUktTEcxcVk'
+MNIST_multi_id = '0B4jlyZGFzRIJZlkzXzFmZnVpUVE'
 MNIST_multi_name = 'MNIST_multi.hdf5'
 
 SVHN_multi_box_id = '0B4jlyZGFzRIJOHRRMy1ISjVxbEE'
 SVHN_multi_box_name = 'SVHN_multi_box.hdf5'
 
-SVHN_multi_id = '0B4jlyZGFzRIJZmhENFhDVEliSEE'
+SVHN_multi_id = '0B4jlyZGFzRIJRUQwMWJfN3laQ3M'
 SVHN_multi_name = 'SVHN_multi.hdf5'
 
 pickle_location = 'datasets/pickles/'
@@ -71,7 +71,7 @@ print()
 user_input = input("Please choose method to get Data. \ni.e. Write 1 or 2 depending on your choice    : ")
 if user_input == '1' :
     print('   ')
-    print('SVHN_new_data_struct.pickle will be automatically formed during the process')
+    # print('SVHN_new_data_struct.pickle will be automatically formed during the process')
     print('   ')
     print('------------Processing MNIST_multi.hdf5------------')
     print('---------------------------------------------------')
@@ -79,12 +79,12 @@ if user_input == '1' :
     print('-------------MNIST_multi.hdf5 Complete-------------')
     print('   ')
     print('   ')
-    print('----------Processing SVHN_multi_box.hdf5-----------')
-    print('---------------------------------------------------')
-    prep_svhn_multi_box()
-    print('----------SVHN_multi_box.hdf5 Completed------------')
-    print('   ')
-    print('   ')
+    # print('----------Processing SVHN_multi_box.hdf5-----------')
+    # print('---------------------------------------------------')
+    # prep_svhn_multi_box()
+    # print('----------SVHN_multi_box.hdf5 Completed------------')
+    # print('   ')
+    # print('   ')
     print('------------Processing SVHN_multi.hdf5-------------')
     print('---------------------------------------------------')
     prep_svhn_multi()
@@ -110,8 +110,8 @@ else :
     print('Downloading SVHN_multi_box.hdf5')
     download_file_from_google_drive(SVHN_multi_box_id, pickle_location + SVHN_multi_box_name)
     print('   ')
-    print('Downloading SVHN_multi.hdf5')
-    download_file_from_google_drive(SVHN_multi_id, pickle_location + SVHN_multi_name)
-    print('   ')
+    # print('Downloading SVHN_multi.hdf5')
+    # download_file_from_google_drive(SVHN_multi_id, pickle_location + SVHN_multi_name)
+    # print('   ')
     print('All Downloads Completed.')
     print('------------------------')
