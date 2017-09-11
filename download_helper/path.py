207e58dbe2ecbf49787c6f538959d7703b021d17
Himanshu Babal <babal.himanshu@gmail.com>
2017-09-11 14:58:47 +0000

    Mnist - tf

--- download_helper/path.py
+++ download_helper/path.py
@@ -1,3 +1,19 @@
+import os
+
+
+gcloud_dir = '/home/himanshubabal/Google-Street-View-House-Numbers/datasets/'
+external_hdd = '/Volumes/700_GB/Study/SVHN/SVHN-Full_Dataset/'
+mac_dir = '/Users/himanshubabal/Google Drive/Himanshu - 1 Drive/ML/Assignments/Udacity/ML Nanodegree Projects/P5 Digit Recognition/datasets/'
+
+if os.path.exists(gcloud_dir):
+    data_dir = gcloud_dir
+
+elif os.path.exists(external_hdd):
+    data_dir = external_dir
+
+else:
+    data_dir = mac_dir
+
 # Google Cloud
 # data_dir = '/home/himanshubabal/Google-Street-View-House-Numbers/datasets/'
 
@@ -5,4 +21,4 @@
 # data_dir = '/Users/himanshubabal/Google Drive/Himanshu - 1 Drive/ML/Assignments/Udacity/ML Nanodegree Projects/P5 Digit Recognition/datasets/'
 
 # External Hard Drive
-data_dir = '/Volumes/700_GB/Study/SVHN/SVHN-Full_Dataset/'
+# data_dir = '/Volumes/700_GB/Study/SVHN/SVHN-Full_Dataset/'
